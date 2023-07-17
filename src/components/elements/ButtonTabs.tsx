15aab207a8333bfad7c2588d5af320df22e0b530
Chester Sim <chestersimdy@gmail.com>
2023-07-17 23:50:40 +0800

    refactor: improve buttontabs hover state

--- src/components/elements/ButtonTabs.tsx
+++ src/components/elements/ButtonTabs.tsx
@@ -15,10 +15,11 @@ const ButtonTab = ({
 	return (
 		<div
 			className={twMerge(
-				'bg-black py-3 text-center cursor-pointer transition duration-300 border border-container-border w-full -mx-[1px] first:mx-0',
+				'bg-black py-3 text-center cursor-pointer transition duration-300 border border-x-0 first:border-l last:border-r border-container-border w-full',
+				'[&+div]:hover:border-container-border-hover [&+div]:hover:bg-container-border-hover', // pseudo divider hover
 				'hover:border-container-border-hover hover:bg-container-bg-hover hover:text-black active:border-container-border-selected', // element states
 				selected &&
-					'border-container-border-selected bg-container-bg-selected text-text-selected z-10',
+					'border-container-border-selected bg-container-bg-selected text-text-selected [&+div]:border-container-border-selected [&+div]:bg-container-border-selected',
 				className
 			)}
 			onClick={onSelect}
@@ -38,8 +39,20 @@ function ButtonTabs({
 	tabClassName?: string;
 }) {
 	const renderTabs = () => {
-		return tabs.map((tab) => (
-			<ButtonTab key={tab.label} {...tab} className={tabClassName} />
+		return tabs.map((tab, index) => (
+			<>
+				{/* pseudo divider used to simulate border colors on element states (e.g. hover, selected) */}
+				{index !== 0 && (
+					<div
+						className={twMerge(
+							'w-[1px] border-y flex-1 min-w-[1px] bg-container-border border-container-border transition duration-300',
+							tab.selected &&
+								'bg-container-border-selected border-container-border-selected'
+						)}
+					/>
+				)}
+				<ButtonTab key={tab.label} {...tab} className={tabClassName} />
+			</>
 		));
 	};
 
