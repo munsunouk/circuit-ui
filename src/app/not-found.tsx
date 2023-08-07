eecd568000e64b93e78499ba804e6e61b9f4cf49
Chester Sim <chestersimdy@gmail.com>
2023-08-07 18:12:03 +0800

    feat: style error and 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,28 +1,15 @@
-'use client';
-
 import Link from 'next/link';
-import { useEffect } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 import Button from '@/components/elements/Button';
 
 import { syne } from '@/constants/fonts';
 
-export default function Error({
-	error,
-	reset,
-}: {
-	error: Error;
-	reset: () => void;
-}) {
-	useEffect(() => {
-		console.error(error);
-	}, [error]);
-
+export default function NotFound() {
 	return (
 		<div className="w-full pt-[40%] md:pt-[20%] text-center flex flex-col gap-4 items-center">
 			<span className={twMerge(syne.className, 'text-xl md:text-4xl')}>
-				Something went wrong
+				404 Not Found
 			</span>
 			<Button className="max-w-[300px] w-full py-3">
 				<Link href="/">Return Home</Link>
