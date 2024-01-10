f55dd66a337699b448fe830b8db2cafc0b94e358
Chester Sim <chestersimdy@gmail.com>
2024-01-10 21:47:20 +0800

    refactor: improve loading speeds

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -196,3 +196,37 @@ export function getBasePnlHistoryFromVaultDeposits(
 
 	return basePnlHistory;
 }
+
+export function hexToHue(hex: string) {
+	let r = parseInt(hex.slice(1, 3), 16) / 255,
+		g = parseInt(hex.slice(3, 5), 16) / 255,
+		b = parseInt(hex.slice(5, 7), 16) / 255;
+
+	let max = Math.max(r, g, b),
+		min = Math.min(r, g, b);
+	let h,
+		s,
+		l = (max + min) / 2;
+
+	if (max === min) {
+		h = s = 0; // achromatic
+	} else {
+		let d = max - min;
+		s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+		switch (max) {
+			case r:
+			default:
+				h = (g - b) / d + (g < b ? 6 : 0);
+				break;
+			case g:
+				h = (b - r) / d + 2;
+				break;
+			case b:
+				h = (r - g) / d + 4;
+				break;
+		}
+		h /= 6;
+	}
+
+	return h * 360; // Return hue in degrees
+}
